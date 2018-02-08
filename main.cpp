3c2849f6fd6f814d546c90970b992e230a7cfaa6
Александр Чех <support@mobiledevelop.ru>
2018-02-08 16:01:34 +0300

    2. Release

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include "matrix/multiplier/algorithm/parallel_outer_algorithm.h"
 #include <matrix/multiplier/algorithm/parallel_inner_algorithm.h>
 #include <matrix/multiplier/algorithm/native_algorithm.h>
-#include "matrix/matrix.h"
 #include "matrix/multiplier/matrix_multiplier.h"
+
+using std::vector;
 
 using fmt::print;
 
@@ -11,27 +12,11 @@ int main() {
     int MATRIX_DIMENSION = 2880;
 
     vector<int> BLOCK_SIZES = {
-            1, 6, 10, 15, 20, 24, 30, 36, 40, 60,
+            6, 10, 15, 20, 24, 30, 36, 40, 60,
             72, 80, 96, 120, 144, 160, 180, 240, 360, 480, 720
     };
 
-    vector<vector<int>> triangle_matrix = {
-            {1, 2, 3,  4},
-            {0, 6, 7,  8},
-            {0, 0, 11, 12},
-            {0, 0, 0,  16}
-    };
-
-    matrix<int> m(4);
-    m.fill(triangle_matrix);
-
-    print("{}\n", m);
-
-    auto b_m = m.to_block_matrix(2);
-
-    print("{}\n", b_m);
-
-    /*const int P = 4;
+    const int P = 4;
     omp_set_num_threads(P);
 
     matrix<int> m1(MATRIX_DIMENSION);
@@ -44,29 +29,30 @@ int main() {
     matrix_multiplier<int, parallel_inner_algorithm> multiplier2{};
     matrix_multiplier<int, parallel_outer_algorithm> multiplier3{};
 
-//    for (int i : BLOCK_SIZES) {
-    auto b_m1 = m1.to_block_matrix(80);
-    auto b_m2 = m2.to_block_matrix(80);
+    for (int i : BLOCK_SIZES) {
+        print("Block size: {}\n", i);
 
-    auto start_time = omp_get_wtime();
-    multiplier2.multiply(b_m1, b_m2);
-    auto end_time = omp_get_wtime() - start_time;
+        auto b_m1 = m1.to_block_matrix(80);
+        auto b_m2 = m2.to_block_matrix(80);
 
-    print("Time inner: {}\n", end_time);
+        auto start_time = omp_get_wtime();
+        multiplier1.multiply(b_m1, b_m2);
+        auto end_time = omp_get_wtime() - start_time;
 
-    start_time = omp_get_wtime();
-    multiplier2.multiply(b_m1, b_m2);
-    end_time = omp_get_wtime() - start_time;
+        print("{}\n", end_time);
 
-    print("Time outer: {}\n", end_time);
+        start_time = omp_get_wtime();
+        multiplier2.multiply(b_m1, b_m2);
+        end_time = omp_get_wtime() - start_time;
 
-    start_time = omp_get_wtime();
-    multiplier1.multiply(b_m1, b_m2);
-    end_time = omp_get_wtime() - start_time;
+        print("Time inner: {}\n", end_time);
 
-    print("Time native: {}\n", end_time);
-//    }
+        start_time = omp_get_wtime();
+        multiplier3.multiply(b_m1, b_m2);
+        end_time = omp_get_wtime() - start_time;
 
-*/
+        print("Time outer: {}\n", end_time);
+    }
+
     return 0;
 }
